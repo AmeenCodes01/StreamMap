17e16bf289cb2bce1e5d40166f3a9b5622d26bd0
AmeenCodes01 <82088892+AmeenCodes01@users.noreply.github.com>
2024-06-05 21:07:50 +0100

    Update livestream.controller.js

--- backend/controllers/livestream.controller.js
+++ backend/controllers/livestream.controller.js
@@ -8,3 +8,12 @@ try{
     res.status(500).json({message:"Error"})
 }
 }
+export const endLive = async (req, res)=>{
+    try{
+
+}catch(e){
+    console.log(e)
+    res.status(500).json({message:"Error"})
+}
+}
+// not always start. when ends, just end. 
