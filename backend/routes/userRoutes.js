941651474816b632a7fbb9b48021f7e75edaee44
AmeenCodes01 <82088892+AmeenCodes01@users.noreply.github.com>
2024-06-12 21:26:17 +0100

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import {getUsers} from "../controllers/user.controller.js";
+import {getUsers, changeCountry} from "../controllers/user.controller.js";
 import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
-router.get("/",  getUsers);
+router.get("/",  getUsers);   ?
+router.post("/changeCountry", changeCountry)
+
 //we could just get the userID from the client or decode cookie using jwt
 export default router;
