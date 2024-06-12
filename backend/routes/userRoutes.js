70902856261facb47c8b7cf3fa0e87a0d00d3d73
AmeenCodes01 <82088892+AmeenCodes01@users.noreply.github.com>
2024-06-12 21:26:36 +0100

    Update userRoutes.js
    added changeCountry routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,5 @@
 import express from "express";
 import {getUsers, changeCountry} from "../controllers/user.controller.js";
-import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
