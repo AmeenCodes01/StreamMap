b6579bb5f87780574983bac650923d587fe49faf
AmeenCodes01 <masaqib5@gmail.com>
2024-06-23 20:29:04 +0100

    sending from laptop to PC. Fixed InSesh timers, Session Table.

--- backend/server.js
+++ backend/server.js
@@ -6,110 +6,41 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import { app, server } from "./socket/socket.js";
+import {app, server} from "./socket/socket.js";
 
 import authRoutes from "./routes/authRoutes.js";
 import sessionRoutes from "./routes/sessionRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import sleepRoutes from "./routes/sleepRoutes.js";
-import scoreRoutes from "./routes/scoreRoutes.js"
-import livestreamRoutes from "./routes/livestreamRoutes.js"
+import scoreRoutes from "./routes/scoreRoutes.js";
+import livestreamRoutes from "./routes/livestreamRoutes.js";
 
 dotenv.config();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-console.log("uri", process.env.MONGO_URI)
-const connectToMongoDB = ()=> {
-    
+console.log("uri", process.env.MONGO_URI);
+const connectToMongoDB = () => {
   mongoose.connect(
-    process.env.MONGO_URI,       
+    process.env.MONGO_URI,
     {useNewUrlParser: true, useUnifiedTopology: true},
-    () => {     
+    () => {
       console.log("Connected to MongoDB");
     }
   );
-  mongoose.set("strictQuery", true)
-}
-
-
-
+  mongoose.set("strictQuery", true);
+};
 
 app.use("/api/auth", authRoutes);
 app.use("/api/sessions", sessionRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/sleep", sleepRoutes);
 app.use("/api/score", scoreRoutes);
-app.use("/api/live", livestreamRoutes)
-<<<<<<< HEAD
-=======
-app.post('/api/refreshToken', (req, res) => {
-  try {
-    
-  //  const token = req.cookies.jwt;
-    
-    // if (!token) {
-      
-    //   return res.status(401).json({error: "Unauthorized - No Token Provided"});
-    // }
-    refreshToken(req,res)
-    
-    // Use the utility function
-  //  res.json({ message:"successfult ref" });
-  } catch (error) {
-    console.error('Error refreshing token:', error);
-    res.status(500).json({ error: 'Token refresh failed' });
-  }
-});
-
-// io.on("connection", (socket) => {
-//   console.log("New user connected " + socket.id);
-
-//   socket.on("join_room", (room) => {
-//     console.log(room);
-//     socket.join(room);
-//     console.log("JOINDE ROOM");
-//     socket.to(room).emit("receive_join_room", "hello");
-//     // socket.join(room);
-//     // io.emit("USER JOINED");
-//   });
-//   socket.on("send_message", (message) => {
-//     console.log(message);
-//     socket.broadcast.emit("receive_message", message);
-//   });
-
-//   socket.on("send-StartGoal", (message) => {
-//     // socket.to(message.room).emit("receive-StartGoal", message); // Broadcast the message to all connected clients
-//     // io.emit("send-StartGoal", message);
-//     socket.broadcast.emit("receive_StartGoal", message);
-//   });
-
-//   socket.on("user_join", (message) => {
-//     // socket.join(message.room);
-//     socket.broadcast.emit("receive_user_join", message);
-
-//     // socket.to(message.room).emit("user-join", message.info); // Broadcast the message to all connected clients
-//   });
-//   // io.socketsJoin("Shamsia");
-
-//   // const sockets = async () => {
-//   //   const s = await io.in("Shamsia").fetchSockets();
-//   //   for (const socket of s) {
-//   //     console.log(socket.rooms);
-//   //   }
-//   // };
-//   // sockets();
-
-//   socket.on("disconnect", () => {
-//     console.log("User disconnected");
-//   });
-// });
->>>>>>> 4881f169b34ebefabd6a9c5b8a0837801725b0f0
+app.use("/api/live", livestreamRoutes);
 
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-	connectToMongoDB();
-	console.log(`Server Running on port ${PORT}`);
+  connectToMongoDB();
+  console.log(`Server Running on port ${PORT}`);
 });
-
--- client/src/components/TimeTable.jsx
+++ client/src/components/TimeTable.jsx
@@ -1,9 +1,8 @@
-import { useState, useEffect, memo } from "react";
-import { Table } from "react-daisyui";
-import { useAuthContext } from "../context/AuthContext";
-import { useSocketContext } from "../context/SocketContext";
-import useGetUsers from "../hooks/useGetUsers"
-
+import {useState, useEffect, memo} from "react";
+import {Table} from "react-daisyui";
+import {useAuthContext} from "../context/AuthContext";
+import {useSocketContext} from "../context/SocketContext";
+import useGetUsers from "../hooks/useGetUsers";
 
 const UserTimes = memo((c) => {
   return (
@@ -19,114 +18,110 @@ const UserTimes = memo((c) => {
   );
 });
 
-
-function TimeTable({  toggleVisible }) {
+function TimeTable({toggleVisible}) {
   const [render, setRender] = useState(false);
   const [sameUsers, setSameUsers] = useState([]);
 
-//   console.log("sameUsers",sameUsers)
-//   useEffect(() => {
-//     let interval = null;
-//     interval = setInterval(() => {
-//       setRender(!render);
-//     }, 1000 * 55);
+  //   console.log("sameUsers",sameUsers)
+  //   useEffect(() => {
+  //     let interval = null;
+  //     interval = setInterval(() => {
+  //       setRender(!render);
+  //     }, 1000 * 55);
 
-//     return () => {
-//       clearInterval(interval);
-//     };
-//   }, [render]);
-//   //Add all joined room as array to users.
+  //     return () => {
+  //       clearInterval(interval);
+  //     };
+  //   }, [render]);
+  //   //Add all joined room as array to users.
 
-//   function getCurrentTimeInTimeZone(timeZone) {
-//     // Create a new Date object with the current time
-//     const currentTime = new Date();
-  
-//     // Format the current time for the specified time zone
-//     const formatter = new Intl.DateTimeFormat('en-US', {
-//       timeZone,
-//       hour: 'numeric',
-//       minute: 'numeric',
-//       hour12: false // Use 24-hour format to avoid AM/PM confusion
-//     });
-  
-//     // Use formatToParts to get the individual parts of the formatted time
-//     const parts = formatter.formatToParts(currentTime);
-  
-//     // Extract hours and minutes from the parts
-//     let hours, minutes;
-//     for (const part of parts) {
-//       if (part.type === 'hour') {
-//         hours = part.value;
-//       } else if (part.type === 'minute') {
-//         minutes = part.value;
-//       }
-//     }
-  
-//     // Return the formatted time as HH:MM
-//     return `${hours}:${minutes}`;
-// }
+  //   function getCurrentTimeInTimeZone(timeZone) {
+  //     // Create a new Date object with the current time
+  //     const currentTime = new Date();
 
-// // Example usage
+  //     // Format the current time for the specified time zone
+  //     const formatter = new Intl.DateTimeFormat('en-US', {
+  //       timeZone,
+  //       hour: 'numeric',
+  //       minute: 'numeric',
+  //       hour12: false // Use 24-hour format to avoid AM/PM confusion
+  //     });
 
+  //     // Use formatToParts to get the individual parts of the formatted time
+  //     const parts = formatter.formatToParts(currentTime);
 
+  //     // Extract hours and minutes from the parts
+  //     let hours, minutes;
+  //     for (const part of parts) {
+  //       if (part.type === 'hour') {
+  //         hours = part.value;
+  //       } else if (part.type === 'minute') {
+  //         minutes = part.value;
+  //       }
+  //     }
 
-  
-//   const { authUser } = useAuthContext();
-//   const { onlineUsers } = useSocketContext();
-  const { users, loading } = useGetUsers();
-  console.log("Users", users)
-//   // positive negative, negative negative, positive positive
-//   // -7 , 5  , 5.5
-//   const timeDiff = (authOffset, offset) => {
-//     //both negative
-//     let diff = parseFloat(authOffset) - parseFloat(offset);
-//     diff = diff < 0 ? diff * -1 : diff;
-//     return `${parseInt(diff)}h ${(diff - parseInt(diff)) * 60}min`;
-//   };
+  //     // Return the formatted time as HH:MM
+  //     return `${hours}:${minutes}`;
+  // }
 
-//   //SHOW OFFSET FROM USER CXOUNTRY. SHAMS AT TOP. SHOW NAME, STYLE IMAGES.
-//   useEffect(() => {
-//     if (onlineUsers[0] !== undefined && users.length !== 0){
+  // // Example usage
 
-//     console.log("onlineUsers", onlineUsers)
-//     const online = onlineUsers.map((ou) => {
-//       let onlineuser;
-//       users.map((u) => {
-//         if (ou === u._id) {
-//           onlineuser = u;
-//         }
-//       });
-//       return onlineuser;
-//     });
-    
-// if (online[0] !== undefined){
+  //   const { authUser } = useAuthContext();
+  //   const { onlineUsers } = useSocketContext();
+  const {users, loading} = useGetUsers();
+  console.log("Users", users);
+  //   // positive negative, negative negative, positive positive
+  //   // -7 , 5  , 5.5
+  //   const timeDiff = (authOffset, offset) => {
+  //     //both negative
+  //     let diff = parseFloat(authOffset) - parseFloat(offset);
+  //     diff = diff < 0 ? diff * -1 : diff;
+  //     return `${parseInt(diff)}h ${(diff - parseInt(diff)) * 60}min`;
+  //   };
 
-//   let singleCountries = online?.map((x) => x.country);
-//   singleCountries = [...new Set(singleCountries)];
-  
-//   const sameUsers = singleCountries.map((c) => {
-//     let users = [];
-//     let timeZone;
-//     online.map((x) => {
-//       if (x.country === c) {
-//         timeZone = x.timeZone;
-//         users.push({
-//           name: x.name,
-//           picture: x.profilePic,
-//         });
-//       }
-//     });
-//     return { users: users,  timeZone: timeZone };
-//   });
-  
-//   setSameUsers(sameUsers);
-// }}
-// console.log(sameUsers)
-// }, [onlineUsers, users]);
+  //   //SHOW OFFSET FROM USER CXOUNTRY. SHAMS AT TOP. SHOW NAME, STYLE IMAGES.
+  //   useEffect(() => {
+  //     if (onlineUsers[0] !== undefined && users.length !== 0){
 
-if (loading){
-  return;
-}
+  //     console.log("onlineUsers", onlineUsers)
+  //     const online = onlineUsers.map((ou) => {
+  //       let onlineuser;
+  //       users.map((u) => {
+  //         if (ou === u._id) {
+  //           onlineuser = u;
+  //         }
+  //       });
+  //       return onlineuser;
+  //     });
+
+  // if (online[0] !== undefined){
+
+  //   let singleCountries = online?.map((x) => x.country);
+  //   singleCountries = [...new Set(singleCountries)];
+
+  //   const sameUsers = singleCountries.map((c) => {
+  //     let users = [];
+  //     let timeZone;
+  //     online.map((x) => {
+  //       if (x.country === c) {
+  //         timeZone = x.timeZone;
+  //         users.push({
+  //           name: x.name,
+  //           picture: x.profilePic,
+  //         });
+  //       }
+  //     });
+  //     return { users: users,  timeZone: timeZone };
+  //   });
+
+  //   setSameUsers(sameUsers);
+  // }}
+  // console.log(sameUsers)
+  // }, [onlineUsers, users]);
+
+  if (loading) {
+    return;
+  }
 
   return (
     <div className="max-h-[75vh] overflow-auto pt-[20px] ">
@@ -145,70 +140,60 @@ if (loading){
         <polygon points="160,128.4 192.3,96 352,256 352,256 352,256 192.3,416 160,383.6 287.3,256 " />
       </svg>
 
-
-      { Object.keys(users).map((country) => {
-        
+      {Object.keys(users).map((country) => {
         return (
-              <div className=" mb-10 bg-base-200">
-                <div
-                  className="flex flex-row w-[100%]  border-2 border-white "
-                  style={{
-                     backgroundColor: users[country][0].color,
-                  }}
-                >
-                  <h6
-                    className="text-md bg-white ml-[10px]  px-2 text-start    "
-                    style={
-                      {
-                                    color: users[country][0].color,
-                      }
-                    }
-                  >
-                    {country}
-                  </h6>
-                </div>
-{/* what if same */}
-
-                <p className=" flex flex-row     ">
-                  <p className="badge  badge-md badge-primary rounded-[0.2px]">
-                    {/* {getCurrentTimeInTimeZone(c.timeZone)} */}
-                  </p>
-                  {/* <span className="ml-[auto] badge  rounded-[2px] badge-md ">
+          <div className=" mb-10 bg-base-200">
+            <div
+              className="flex flex-row w-[100%]  border-2 border-white "
+              style={{
+                backgroundColor: users[country][0].color,
+              }}
+            >
+              <h6
+                className="text-md bg-white ml-[10px]  px-2 text-start    "
+                style={{
+                  color: users[country][0].color,
+                }}
+              >
+                {country}
+              </h6>
+            </div>
+            {/* what if same */}
+            <p className=" flex flex-row     ">
+              <p className="badge  badge-md badge-primary rounded-[0.2px]">
+                {/* {getCurrentTimeInTimeZone(c.timeZone)} */}
+              </p>
+              {/* <span className="ml-[auto] badge  rounded-[2px] badge-md ">
                 {timeDiff(authUser.offset, c.offset)}
                   </span> */}
+            </p>
+            <div>
+              {users[country].map((x) => (
+                <li className="flex flex-col gap-[12px] ">
+                  <ul className="  flex flex-row px-[10px] py-[15px]">
+                    <div className="avatar">
+                      <div className="w-8 rounded-full ring ring-primary  ring-offset-base-100 ring-offset-1">
+                        <img
+                          src={`${country.profilePicture}`}
+                          className=" w-[38px] h-[38px] rounded-[100px]   "
+                        />
+                      </div>
+                      <p className="prose prose-md self-center ml-[10px] ">
+                        {x.name}
+                      </p>
+                      {/* <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" /> */}
+                    </div>
+                  </ul>
+                </li>
+              ))}
+            </div>{" "}
+          </div>
+        );
 
-                </p>
-                <div>
-                  {
-                    ? c?.users?.map((x) => (
-                        <li className="flex flex-col gap-[12px] ">
-                          <ul className="  flex flex-row px-[10px] py-[15px]">
-                            <div className="avatar">
-                              <div className="w-8 rounded-full ring ring-primary  ring-offset-base-100 ring-offset-1">
-                                <img
-                                  src={`${x.picture}`}
-                                  //                          className=" w-[38px] h-[38px] rounded-[100px]   "
-                                />
-                              </div>
-                              <p className="prose prose-md self-center ml-[10px] ">
-                                {x.name}
-                              </p>
-                              {/* <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" /> */}
-                            </div>
-                          </ul>
-                         
-                        </li>
-                      ))
-                    }
-                </div>              </div>
-            );
-
-            {
-              /* </div> */
-            }
-          })
+        {
+          /* </div> */
         }
-     
+      })}
     </div>
   );
 }
