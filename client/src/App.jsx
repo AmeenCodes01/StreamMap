04fae13172fa0284674c668ee95a37a2d4aeff74
AmeenCodes01 <masaqib5@gmail.com>
2024-07-09 12:07:25 +0100

    blank

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -37,7 +37,6 @@ function App() {
           <Route path="allUsers" element={<AllUsers />} />
         </Route>
       </Routes>
-<<<<<<< Updated upstream
       <Toaster
         containerClassName="z-[100000000000000000000]"
         toastOptions={{
@@ -47,18 +46,6 @@ function App() {
           },
         }}
       />
-=======
-      <Toaster toastOptions={{
-        
-        // style:{
-        //   zIndex: "111000000000000000000",
-        //   position:"absolute", 
-        //   top:"200px",
-        //   right: "150px"
-
-        // }
-      }} containerStyle={{ zIndex: "10000000"}} />
->>>>>>> Stashed changes
     </BrowserRouter>
   );
 }
