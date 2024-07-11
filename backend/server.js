baa900132b6984d7214ef3fb53f274c9cc9dc8bd
AmeenCodes01 <masaqib5@gmail.com>
2024-07-11 23:17:55 +0100

    updated server routes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,10 @@
-// const express = require("express");
 import express from "express";
-// import {createServer} from "http";
-// import {Server} from "socket.io";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
+import {fileURLToPath} from "url";
 import {app, server} from "./socket/socket.js";
 
 import authRoutes from "./routes/authRoutes.js";
@@ -16,7 +15,9 @@ import scoreRoutes from "./routes/scoreRoutes.js";
 import livestreamRoutes from "./routes/livestreamRoutes.js";
 import promisesRoutes from "./routes/promisesRoutes.js";
 import countryRoutes from "./routes/countryRoutes.js";
+
 dotenv.config();
+
 app.use(
   cors({
     origin: "https://streammap-frontend.onrender.com",
@@ -24,8 +25,10 @@ app.use(
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(cookieParser());
+
 const connectToMongoDB = () => {
   mongoose.connect(
     process.env.MONGO_URI,
@@ -44,8 +47,20 @@ app.use("/api/live", livestreamRoutes);
 app.use("/api/promise", promisesRoutes);
 app.use("/api/country", countryRoutes);
 
+// Serve static files from the React app
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
+
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   connectToMongoDB();
+  console.log(`Server running on port ${PORT}`);
 });
